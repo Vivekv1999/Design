9013985adf32d7ea0d2bae566deafc5f9d8caff6
vivek <vivekvekariya1999@gmail.com>
2024-01-08 22:28:09 +0530

    add image slider with issue in slider

--- policy_bazar-ui-clone/src/Component/AlsoBuy/AlsoBySection.js
+++ policy_bazar-ui-clone/src/Component/AlsoBuy/AlsoBySection.js
@@ -1,85 +1,92 @@
 import React from "react";
 
-const sectionCardList=[
+const sectionCardList = [
   {
-    label:"LIC Plans",
-    subHeader:"investment",
-    subHeaderColor:""
+    label: "LIC Plans",
+    subHeader: "investment",
+    subHeaderColor: "",
   },
   {
-    label:"Return of Premium",
-    subHeader:"Term Life",
-    subHeaderColor:""
+    label: "Return of Premium",
+    subHeader: "Term Life",
+    subHeaderColor: "",
   },
   {
-    label:"Life Insurance of HouseWives",
-    subHeader:"investment",
-    subHeaderColor:""
+    label: "Life Insurance of HouseWives",
+    subHeader: "investment",
+    subHeaderColor: "",
   },
   {
-    label:"Day 1 of Coveraje",
-    subHeader:"Health Insurance",
-    subHeaderColor:""
+    label: "Day 1 of Coveraje",
+    subHeader: "Health Insurance",
+    subHeaderColor: "",
   },
   {
-    label:"1CR Health Insurance",
-    subHeader:"Health Insurance",    
-    subHeaderColor:""
+    label: "1CR Health Insurance",
+    subHeader: "Health Insurance",
+    subHeaderColor: "",
   },
   {
-    label:"Unlimited Restoration of Cover",
-    subHeader:"Health Insurance",
-    subHeaderColor:""
+    label: "Unlimited Restoration of Cover",
+    subHeader: "Health Insurance",
+    subHeaderColor: "",
   },
   {
-    label:"Return of Premium",
-    subHeader:"Term Life",
-    subHeaderColor:""
+    label: "Return of Premium",
+    subHeader: "Term Life",
+    subHeaderColor: "",
   },
   {
-    label:"Life Insurance of HouseWives",
-    subHeader:"investment",
-    subHeaderColor:""
+    label: "Life Insurance of HouseWives",
+    subHeader: "investment",
+    subHeaderColor: "",
   },
   {
-    label:"Day 1 of Coveraje",
-    subHeader:"Health Insurance",
-    subHeaderColor:""
+    label: "Day 1 of Coveraje",
+    subHeader: "Health Insurance",
+    subHeaderColor: "",
   },
   {
-    label:"1CR Health Insurance",
-    subHeader:"Health Insurance",    
-    subHeaderColor:""
+    label: "1CR Health Insurance",
+    subHeader: "Health Insurance",
+    subHeaderColor: "",
   },
   {
-    label:"Unlimited Restoration of Cover",
-    subHeader:"Health Insurance",
-    subHeaderColor:""
+    label: "Unlimited Restoration of Cover",
+    subHeader: "Health Insurance",
+    subHeaderColor: "",
   },
-]
+];
 
 const AlsoBySection = () => {
   return (
     <div className="app_main_wrapper p-4">
       <div className="flex justify-start flex-col px-40 ">
         <h2 className="text-[14px] font-medium block text-blue-700 mb-0">
-            Also Buy
+          Also Buy
         </h2>
-        <br/>
-        <div className="flex flex-3"> 
-        {sectionCardList.map((d)=>{
+        <br />
+        <div className="flex flex-3">
+          <card className="justify-start" style={{boxShadow:"1px 2px 2px 10px rgba(0, 0, 255, .2)"}}>
+            <div className="text-[11px] flex">
+              <span>.</span>
+              <span>investment</span>
+              </div>
+            <div className="text-[16px]">LIC Plans</div>
+          </card>
+          {/* {sectionCardList.map((d)=>{
        return <section className="bg-white shadow-[10px_15px_10px_2px_rgba(143,131,131,0.4)] border-solid border-2 border-indigo-600
         p-2 rounded-2xl items-center 
         ">
           <div className="items-center text-sm flex align-middle p-0">
             <span className="w-1 rounded-2xl">.</span>
-            <div className="text-[12px]">
+            <div className="text-[12px] w-full">
             {d.label}
             </div>
           </div>
           <div className="items-center text-center text-[15px]">LIC Plans</div>
         </section>
-        })}
+        })} */}
         </div>
       </div>
     </div>
--- policy_bazar-ui-clone/src/Component/SliderImage/SliderImage.js
+++ policy_bazar-ui-clone/src/Component/SliderImage/SliderImage.js
@@ -1,10 +1,104 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+
+const sliderImage = [
+  {
+    image: "assets/Slider_image/a1.jfif",
+    name: "First Image",
+    backgroundColor: "#0088FE"
+  },
+  {
+    image: "assets/Slider_image/a2.jfif",
+    name: "Second Image",
+    backgroundColor: "#00C49F"
+  },
+  {
+    image: "assets/Slider_image/a3.jfif",
+    name: "Thirt Image",
+    backgroundColor: "#FFBB28"
+  },
+  {
+    image: "assets/Slider_image/a4.jfif",
+    name: "Fourth Image",
+    backgroundColor: "#0088FE"
+  },
+  {
+    image: "assets/Slider_image/a5.png",
+    name: "Fifth Image",
+    backgroundColor: "#00C49F"
+  },
+  {
+    image: "assets/Slider_image/a6.jfif",
+    name: "Sixth Image",
+    backgroundColor: "#FFBB28"
+  },
+  {
+    image: "assets/Slider_image/a7.png",
+    name: "Sixth Image",
+    backgroundColor: "#FFBB28"
+  },
+];
 
 const SliderImage = () => {
+
+  const [index, setIndex] = useState(0);
+  const timeoutRef = useRef(null);
+  const delay = 2500;
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === sliderImage.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
+
   return (
-    <div>
-      
+    <div className="app_main_wrapper app_navbar p-4">
+    <div className="flex justify-between items-center px-40">
+      <div className="w-full">
+        <div className="slideshow">
+          <div
+            className="slideshowSlider"
+            style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+          >
+            {sliderImage.map((item, index) => (
+              <div
+                className="slide"
+                key={index}
+                style={{ backgroundColor:item.backgroundColor,borderRadius:"40px"}}
+              >
+              <img src={item.image} alt={item.name} style={{padding:"10px" }}/>
+              </div>
+            ))}
+          </div>
+          <div className="slideshowDots">
+            {sliderImage.map((_, idx) => (
+              <div
+                key={idx}
+                className={`slideshowDot${index === idx ? " active" : ""}`}
+                onClick={() => {
+                  setIndex(idx);
+                }}
+              ></div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
   )
 }
 
